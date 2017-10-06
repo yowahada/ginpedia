5957333ba73a9f9e9847314ff919f31cbc0bbe0a
ohashi-t <j07018@gmail.com>
2017-10-06 17:06:40 +0900

    add css folder

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
