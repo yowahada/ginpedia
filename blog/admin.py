9528e530d57cf170e45b82381250bf95b6f8132c
ohashi-t <j07018@gmail.com>
2017-10-17 13:51:33 +0900

    modified html

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-from import_export import resources
 from .models import Post
-from import_export.admin import ImportExportModelAdmin
-from import_export import fields
 
 admin.site.register(Post)
