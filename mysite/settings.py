88989225d348d00651999cdc78d269cc1fc788cb
ohashi-t <j07018@gmail.com>
2017-10-17 15:35:33 +0900

    change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,10 +121,13 @@ USE_TZ = False
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, "static"), 
 )
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
