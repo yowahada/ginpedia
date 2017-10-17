7b8e2a084ba01b15e68b4996cf4d3179a6fe5b87
ohashi-t <j07018@gmail.com>
2017-10-17 13:24:46 +0900

    modified html

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from import_export import resources
 from .models import Post
+from import_export.admin import ImportExportModelAdmin
+from import_export import fields
 
 admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,12 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    flavor_text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    url = models.TextField(null=True)
 
     def publish(self):
         self.published_date = timezone.now()
