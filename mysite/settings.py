bc0e9ba0718feec839d00af797df619cc1a0648b
ohashi-t <j07018@gmail.com>
2017-10-18 13:56:40 +0900

    change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 import dj_database_url
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
