9c0d079431eb9565c252e6f58713fa7e451b31b6
ohashi-t <j07018@gmail.com>
2017-10-19 19:07:12 +0900

    change DB&html

--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,17 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
+    Distillery = models.CharField(max_length=200,null=True)
+    ABV = models.CharField(max_length=200,null=True)
+    Estimated_price = models.CharField(max_length=200,null=True)
     flavor_text = models.TextField()
+    Tasting_note = models.TextField(null=True)
+    Cocktails = models.TextField(null=True)
+    url = models.URLField(max_length=200,null=True)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
-    url = models.TextField(null=True)
 
     def publish(self):
         self.published_date = timezone.now()
