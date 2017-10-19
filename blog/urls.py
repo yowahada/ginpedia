4e580e9f166056a7b4587477e400ca3158bfbe98
ohashi-t <j07018@gmail.com>
2017-10-19 14:27:50 +0900

    change .html

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.post_list, name='post_list'),
+	url(r'^$', views.post_list, name='gin_list'),
+	url(r'(?P<pk>\d+)/$', views.index, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.http import HttpResponse
 
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html',{'posts':posts})
+	return render(request, 'blog/gin_list.html',{'posts':posts})
+
+def index(request, pk):
+	return render(request,'blog/detail.html',{
+		'post':Post.objects.get(pk=pk)
+		})
