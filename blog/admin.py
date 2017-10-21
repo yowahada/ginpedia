66057239dc5a0acec26029b8102c50efb24808d1
yowahada <development.tskz49@gmail.com>
2017-10-21 22:16:19 +0900

    add fileupdater

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-	list_display = ('title','Distillery','ABV','admin_image')
+	list_display = ('title','Distillery','ABV')
+	#,'admin_image'
 
 admin.site.register(Post, PostAdmin)
