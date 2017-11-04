d86c26313edccaa6f82b49a7cddb0d4c26f3fd47
yowahada <development.tskz49@gmail.com>
2017-11-04 19:08:13 +0900

    merge

--- blog/models.py
+++ blog/models.py
@@ -7,14 +7,13 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     Distillery = models.CharField(max_length=200,blank=True)
     ABV = models.CharField(max_length=200,null=True,default='comming soon')
+    Country = models.CharField(max_length=100, blank=True)
     Estimated_price = models.CharField(max_length=200,null=True ,default='comming soon')
-    flavor_text = models.TextField(default='comming soon')
+    Flavor_text = models.TextField(null=True, default='comming soon')
+    Botanical = models.TextField(default='comming soon')
     Tasting_note = models.TextField(null=True, default='comming soon')
-    Cocktails = models.TextField(null=True, default='comming soon')
     url = models.URLField(
             blank=True, max_length=200,null=True)
-    #image = models.ImageField(
-    #        blank=True ,upload_to='document/')
     image = models.ImageField(
             blank=True ,upload_to='document/',default='settings.MEDIA_ROOT/document/404.png')
     created_date = models.DateTimeField(
