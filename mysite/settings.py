7a2dc62fec52ae46ea3b444528a75b9cd0a8a222
yowahada <development.tskz49@gmail.com>
2017-11-24 19:43:27 +0900

    settings.pyコメント修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ if 'local' in HOSTNAME:
 else:
     SECRET_KEY = os.environ['SECRET_KEY']
 
-    # aws設定
+    # aws基本設定設定
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
@@ -33,10 +33,13 @@ else:
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
+
+    # staticファイルもS3に移す場合はこっち
     # AWS_LOCATION = 'static'
     # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
+    # 本番mediaファイルのみS3対応
     STATIC_URL = 'https://%s/' % (AWS_S3_CUSTOM_DOMAIN)
     DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
 
@@ -172,6 +175,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"), 
 )
 
+# ローカル用mediaフォルダ
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (
     os.path.join(BASE_DIR, 'media'))
