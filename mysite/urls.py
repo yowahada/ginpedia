78a8f48a62669e319c9d2a2133b5af6fa450af7b
yowahada <development.tskz49@gmail.com>
2017-12-08 18:45:51 +0900

    url.py修正

--- mysite/urls.py
+++ mysite/urls.py
@@ -32,8 +32,6 @@ urlpatterns = [
     url(r'^blog/',include('article.urls',namespace='article')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
