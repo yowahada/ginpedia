2f37f8688fa9ff09d41887bd057eb3367bdd001c
yowahada <development.tskz49@gmail.com>
2017-12-12 16:05:46 +0900

    domain修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,7 +50,7 @@ else:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['cocktail-parties.com']
+ALLOWED_HOSTS = ['www.cocktail-parties.com']
 DEBUG = False
 
 
