9d817cae07591f93936e05a8d8aab82814e38a83
yowahada <development.tskz49@gmail.com>
2017-12-12 16:00:35 +0900

    ALLOWED_HOSTS変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,7 +50,7 @@ else:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['ginpedia.herokuapp.com']
+ALLOWED_HOSTS = ['cocktail-parties.com']
 DEBUG = False
 
 
